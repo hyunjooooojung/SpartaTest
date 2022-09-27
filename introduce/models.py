d49a01600ec402629bc0fa321dd5f54dd6a3291d
hyunjoojung <hyunjoo3107@gmail.com>
2022-09-27 09:22:43 +0900

    accesslog db테이블 생성

--- introduce/models.py
+++ introduce/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class UserModel(models.Model):
+    class AccessLog:
+        db_table = "AccessLog"
+
+        created_at = models.DateTimeField(auto_now_add=True)
+        location = models.CharField(max_length=256, null=False)
