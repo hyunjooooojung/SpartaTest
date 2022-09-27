a75f1b39d478931185e4cf697cfa8dd9577988dc
hyunjoojung <hyunjoo3107@gmail.com>
2022-09-27 10:24:14 +0900

    db테이블 수정

--- introduce/models.py
+++ introduce/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class UserModel(models.Model):
-    class AccessLog:
-        db_table = "AccessLog"
+class AccessLog(models.Model):
+    db_table = "AccessLog"
 
-        created_at = models.DateTimeField(auto_now_add=True)
-        location = models.CharField(max_length=256, null=False)
+    created_at = models.DateTimeField("접속시간", auto_now_add=True)
+    location = models.CharField("접속 경로", max_length=50)
