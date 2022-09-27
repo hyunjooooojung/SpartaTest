835a9e6a5f07cdf8334b013905f17c481c3f9797
hyunjoojung <hyunjoo3107@gmail.com>
2022-09-27 22:53:36 +0900

    views.py, models.py 수정

--- introduce/models.py
+++ introduce/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 class AccessLog(models.Model):
-    db_table = "AccessLog"
+    
 
     created_at = models.DateTimeField("접속 시간 : ", auto_now_add=True)
     location = models.CharField("접속 경로 : ", max_length=50)
+
+    def __str__(self):
+        return f"{self.created_at} / {self.location}"
